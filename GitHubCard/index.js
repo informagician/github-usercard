9582d6c448c5a4c3ce1e0bf630bf77176a05d734
Milo Rastgoo <hello@informagician.com>
2019-12-05 18:41:48 -0500

    axios get request + card component creator partially

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+//const axios = require('axios');
 
+axios.get('https://api.github.com/users/informagician')
+.then(function(response){
+  console.log(response)
+})
+.catch(function(err) {
+  console.log(err);
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -13,7 +21,16 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+function cardCreator(data) {
 
+  const card = document.createElement(div),
+        image = document.createElement(img),
+        name = document.createElement(h3),
+        bio = document.createElement(p),
+        username = document.createElement(h4);
+
+  
+}
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
